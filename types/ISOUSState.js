122c6c7f71fb09aaca2b008066465ba216964843
Andrew Carlson <acarl005@g.ucla.edu>
2017-05-23 12:20:00 -0700

    add descriptions

--- types/ISOUSState.js
+++ types/ISOUSState.js
@@ -1,74 +1,77 @@
 const { GraphQLEnumType } = require('graphql')
 
-const CODES =  [
-  'AL', // Alabama
-  'AK', // Alaska
-  'AZ', // Arizona
-  'AR', // Arkansas
-  'CA', // California
-  'CO', // Colorado
-  'CT', // Connecticut
-  'DE', // Delaware
-  'FL', // Florida
-  'GA', // Georgia
-  'HI', // Hawaii
-  'ID', // Idaho
-  'IL', // Illinois
-  'IN', // Indiana
-  'IA', // Iowa
-  'KS', // Kansas
-  'KY', // Kentucky
-  'LA', // Louisiana
-  'ME', // Maine
-  'MD', // Maryland
-  'MA', // Massachusetts
-  'MI', // Michigan
-  'MN', // Minnesota
-  'MS', // Mississippi
-  'MO', // Missouri
-  'MT', // Montana
-  'NE', // Nebraska
-  'NV', // Nevada
-  'NH', // New Hampshire
-  'NJ', // New Jersey
-  'NM', // New Mexico
-  'NY', // New York
-  'NC', // North Carolina
-  'ND', // North Dakota
-  'OH', // Ohio
-  'OK', // Oklahoma
-  'OR', // Oregon
-  'PA', // Pennsylvania
-  'RI', // Rhode Island
-  'SC', // South Carolina
-  'SD', // South Dakota
-  'TN', // Tennessee
-  'TX', // Texas
-  'UT', // Utah
-  'VT', // Vermont
-  'VA', // Virginia
-  'WA', // Washington
-  'WV', // West Virginia
-  'WI', // Wisconsin
-  'WY', // Wyoming
+const CODES = {
+  AL: 'Alabama',
+  AK: 'Alaska',
+  AZ: 'Arizona',
+  AR: 'Arkansas',
+  CA: 'California',
+  CO: 'Colorado',
+  CT: 'Connecticut',
+  DE: 'Delaware',
+  FL: 'Florida',
+  GA: 'Georgia',
+  HI: 'Hawaii',
+  ID: 'Idaho',
+  IL: 'Illinois',
+  IN: 'Indiana',
+  IA: 'Iowa',
+  KS: 'Kansas',
+  KY: 'Kentucky',
+  LA: 'Louisiana',
+  ME: 'Maine',
+  MD: 'Maryland',
+  MA: 'Massachusetts',
+  MI: 'Michigan',
+  MN: 'Minnesota',
+  MS: 'Mississippi',
+  MO: 'Missouri',
+  MT: 'Montana',
+  NE: 'Nebraska',
+  NV: 'Nevada',
+  NH: 'New Hampshire',
+  NJ: 'New Jersey',
+  NM: 'New Mexico',
+  NY: 'New York',
+  NC: 'North Carolina',
+  ND: 'North Dakota',
+  OH: 'Ohio',
+  OK: 'Oklahoma',
+  OR: 'Oregon',
+  PA: 'Pennsylvania',
+  RI: 'Rhode Island',
+  SC: 'South Carolina',
+  SD: 'South Dakota',
+  TN: 'Tennessee',
+  TX: 'Texas',
+  UT: 'Utah',
+  VT: 'Vermont',
+  VA: 'Virginia',
+  WA: 'Washington',
+  WV: 'West Virginia',
+  WI: 'Wisconsin',
+  WY: 'Wyoming',
 
   // districts
-  'DC', // District of Columbia
+  DC: 'District of Columbia',
 
   // outlying areas
-  'AS', // American Samoa
-  'GU', // Guam
-  'MP', // Northern Mariana Islands
-  'PR', // Puerto Rico
-  'UM', // United States Minor Outlying Islands
-  'VI', // Virgin Islands, U.S.
-]
+  AS: 'American Samoa',
+  GU: 'Guam',
+  MP: 'Northern Mariana Islands',
+  PR: 'Puerto Rico',
+  UM: 'United States Minor Outlying Islands',
+  VI: 'Virgin Islands, U.S.',
+}
 exports.CODES = CODES
 
 
 const values = {}
-for (let code of CODES) {
-  values[code] = { value: code }
+for (let code in CODES) {
+  values[code] = {
+    description: CODES[code],
+    value: code
+  }
 }
 
 exports.ISOUSState = new GraphQLEnumType({
